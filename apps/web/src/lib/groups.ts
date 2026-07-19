b21e6dc3712fcbd202849bda5bb90ded3f158984
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-07-19 20:05:30 +0700

    fix(groups): update airline names in groupData

--- apps/web/src/lib/groups.ts
+++ apps/web/src/lib/groups.ts
@@ -3,32 +3,32 @@ import type { Group } from "./type";
 export const groupData: Group[] = [
   {
     number: 1,
-    name: 'โมโนหวงเจิ้น',
+    name: 'โมโนสยาม แอร์เวย์',
     link: "https://line.me/ti/g2/5hCpBYQpbjIKi4cQxOUF9zs2Z1FoZMh-6ZG2aQ"
   },
   {
     number: 3,
-    name: 'SANXiARA',
+    name: 'Trisara airways',
     link: "https://line.me/ti/g2/i25dePoOX8VhF18C8jiW7hyc8PA8RtM0tDBlKQ"
   },
   {
     number: 4,
-    name: 'PINK HU4',
+    name: 'Pink Horizon',
     link: "https://line.me/ti/g2/ICecb3yCIOrA3ZMBzoWn15cME7oA8KUK5Dq3ew"
   },
   {
     number: 5,
-    name: 'Wulong 5',
+    name: 'กล้วยหอมแอร์ไลน์',
     link: "https://line.me/ti/g2/HKkoBCbHCuCRv_OxeQOLQ6kt5cnFwp08oMn4CA"
   },
   {
     number: 6,
-    name: 'หก ลก ซิ่ว',
+    name: 'sixthsenseairline',
     link: "https://line.me/ti/g2/dcNEFSkfbtzYlmXWGi1zQQk9tT_MXv5jH4x3pg"
   },
   {
     number: 7,
-    name: 'เจ็ดโต่วอิน',
+    name: 'สวรรค์ชั้น7แอร์ไลน์',
     link: "https://line.me/ti/g2/GNP2HDqQ3HsAn-42pbCVUVNP9W4F1U_7SmIT1A"
   }
 ];
